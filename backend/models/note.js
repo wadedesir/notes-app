bd7f60a21069222fd9fa5f967ea67788fe3c93a9
MicahMartin <micah.m329@gmail.com>
2024-01-30 08:37:04 -0500

    including date timestamps

--- backend/models/note.js
+++ backend/models/note.js
@@ -1,9 +1,14 @@
 import mongoose from 'mongoose'
 
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
+const noteSchema = new mongoose.Schema(
+  {
+    content: String,
+    important: Boolean,
+  },
+  {
+    timestamps: true
+  }
+)
 
 const Note = mongoose.model('Note', noteSchema)
 export default Note
