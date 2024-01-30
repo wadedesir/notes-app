e487f8509aaed7576d52c9288b6d1993c6ce481a
Wade Desir <wededesir@gmail.com>
2024-01-29 22:03:51 -0500

    added form and styling for button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,15 @@
+import { useState } from 'react'
 import logo from '../assets/logo.png'
+import AddNote from '../components/AddNote'
 
 function Home() {
-  return (
-    <div className='container mx-auto'>
-      <img className="fixed size-40 left-5 top-0 object-contain" src={logo}/>
 
+  const [note, setNote] = useState('')
+
+  return (
+    <div className='container' style={{minWidth: '100vw', maxHeight: '100vh'}}>
+      <img className="fixed size-40 left-5 top-0 object-contain" src={logo}/>
+      <AddNote note={note} setNote={setNote}></AddNote>
     </div>
   )
 }
