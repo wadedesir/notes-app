6e0ea5c42f5eee01d5e41479721612e6b5f474aa
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2024-02-05 18:41:50 -0500

    Resolves lint error (#92)

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true, jest: true },
+  env: { browser: true, es2020: true, jest: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
