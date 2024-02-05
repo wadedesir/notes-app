abd144606f80e82150667c51d56bb92899236bdd
Zrybea Whitfield <66852175+ZrybeaWhitfield@users.noreply.github.com>
2024-02-05 11:44:26 -0500

    Updated tests for BE, 86% coverage NoteController, lint errors resolved

--- backend/tests/getTokenFrom.test.js
+++ backend/tests/getTokenFrom.test.js
@@ -1,22 +1,21 @@
-import { getTokenFrom } from "../controllers/NoteController"
+import { getTokenFrom } from '../controllers/NoteController'
 
-describe("Unit tests for getTokenfunction", () => {
-    const token = '123456789'
+describe('Unit tests for getTokenfunction', () => {
+  const token = '123456789'
 
-    test("when header starts with 'Bearer' token is correctly extracted", () => {
-        const authHeader = `Bearer ${token}`
-        const authorizedUserToken = getTokenFrom(authHeader)
+  test("when header starts with 'Bearer' token is correctly extracted", () => {
+    const authHeader = `Bearer ${token}`
+    const authorizedUserToken = getTokenFrom(authHeader)
 
-        expect(authorizedUserToken).toBe(token);
-    })
+    expect(authorizedUserToken).toBe(token)
+  })
 
-    test("when header does not start with 'Bearer' getTokenFrom returns null", () => {
-        const authHeader = `Invalid ${token}`
-        const invalidFormatToken = getTokenFrom(authHeader)
-        const emptyToken = getTokenFrom('')
+  test("when header does not start with 'Bearer' getTokenFrom returns null", () => {
+    const authHeader = `Invalid ${token}`
+    const invalidFormatToken = getTokenFrom(authHeader)
+    const emptyToken = getTokenFrom('')
 
-        expect(invalidFormatToken).toBeNull
-        expect(emptyToken).toBeNull
-
-    })
+    expect(invalidFormatToken).toBeNull()
+    expect(emptyToken).toBeNull()
+  })
 })
--- backend/tests/note_api_unit.test.js
+++ backend/tests/note_api_unit.test.js
@@ -1,84 +1,43 @@
-import { getTokenFrom, createNewNote } from "../controllers/NoteController"
-import jwt from "jsonwebtoken"
+import { createNewNote } from '../controllers/NoteController'
+import jwt from 'jsonwebtoken'
 
-// jest.mock('jsonwebtoken', () => ({
-//     verify: jest.fn(),
-// }));
-jest.unstable_mockModule('../controllers/NoteController', () => ({
-    getTokenFrom: jest.fn(() => "Mocked token")
-}))
+describe('Unit Tests for Note APIs', () => {
+  describe('Unit Tests for createNewNote', () => {
+    const reqBody = { content: 'Test Note', important: false }
 
-describe("Unit Tests for Note APIs", () => {
-    describe("Unit tests for getTokenfunction", () => {
-        const token = '123456789'
-    
-        test("when header starts with 'Bearer' token is correctly extracted", () => {
-            const authHeader = `Bearer ${token}`
-            const authorizedUserToken = getTokenFrom(authHeader)
-    
-            expect(authorizedUserToken).toBe(token);
-        })
-    
-        test("when header does not start with 'Bearer' getTokenFrom returns null", () => {
-            const authHeader = `Invalid ${token}`
-            const invalidFormatToken = getTokenFrom(authHeader)
-            const emptyToken = getTokenFrom('')
-    
-            expect(invalidFormatToken).toBeNull
-            expect(emptyToken).toBeNull
-    
-        })
+    test('when authorization header is empty, return error', async () => {
+      const req = {
+        body: reqBody,
+        get: jest.fn().mockReturnValue('')
+      }
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnThis()
+      }
+
+      await createNewNote(req, res)
+
+      expect(req.get).toHaveBeenCalledWith('authorization')
+      expect(res.status).toHaveBeenCalledWith(400)
+      expect(res.json).toHaveBeenCalledWith({ error: 'must provide auth token' })
     })
 
-    describe("Unit Tests for createNewNote", () => {
-        beforeAll(async () => {
-            jest.spyOn(jwt, 'verify').mockReturnValue({})
+    test('when decoded token has no id, return error', async () => {
+      const req = {
+        body: reqBody,
+        get: jest.fn().mockReturnValue('Bearer ValidHeader123456789')
+      }
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn().mockReturnThis()
+      }
 
-            const noteController = await import('../controllers/NoteController')
-            mockGetTokenFrom = noteController.getTokenFrom
-        });
+      jest.spyOn(jwt, 'verify').mockReturnValue({ id: undefined })
 
-        afterAll(() => {
-            jest.resetAllMocks()
-        });
+      await createNewNote(req, res)
 
-        const reqBody = {content: "Test Note", important: false}
-
-        test("when authorization header is empty, return error", async () => {
-            const req = {
-                body: reqBody,
-                get: jest.fn().mockReturnValue('')
-            }
-            const res = {
-                status: jest.fn().mockReturnThis(),
-                json: jest.fn().mockReturnThis()
-            }
-
-            await createNewNote(req, res)
-            
-            expect(req.get).toHaveBeenCalledWith('authorization')
-            expect(res.status).toHaveBeenCalledWith(400)
-            expect(res.json).toHaveBeenCalledWith({ error: 'must provide auth token' })
-        })
-
-        //TODO check that correct method is being called 
-        test("when decoded token has no id, return error", async ()  => {
-            const req = {
-                body: reqBody,
-                get: jest.fn().mockReturnValue('')
-            }
-            const res = {
-                status: jest.fn().mockReturnThis(),
-                json: jest.fn().mockReturnThis()
-            }
-
-            jwt.verify.mockReturnValue({})
-            mockGetTokenFrom.mockReturnValue('Token without ID')
-
-            await createNewNote(req, res)
-
-            expect(res.status).toHaveBeenCalledWith(401)
-            expect(res.json).toHaveBeenCalledWith({ error: 'token invalid' })
-        })
+      expect(res.status).toHaveBeenCalledWith(401)
+      expect(res.json).toHaveBeenCalledWith({ error: 'token invalid' })
     })
+  })
 })
