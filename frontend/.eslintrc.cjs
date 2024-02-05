da9820c45348238941af9a44a88a6e4f61461024
Micah Martin <micah.m329@gmail.com>
2024-02-05 14:56:01 -0500

    Issue 71 - add e2e (#72)
    * adding cypress
    
    * adding test route
    
    * updated test route
    
    * Adding build status & license to readme
    
    * adding badges for readme
    
    * remove frontend test badge until we fix lint errors on frontend?
    
    * running cypress
    
    * added cypress
    
    * updated readme to reflect cypress stuff
    
    * added prod url to main readme
    
    * changed link to prod
    
    * ok fixed frontend linting
    
    * ok added frontend badge to readme and fixed frontend lint

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'cypress*', '*.test.js', 'coverage'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
@@ -16,5 +16,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    "react/prop-types": "off",
+    "react/no-unescaped-entities": "off",
   },
 }
